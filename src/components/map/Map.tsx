18825f0d6c58482fdfda1d289ce3f5db702e3eb7
Peter Ondrejka <pondrejk@redhat.com>
2022-04-26 22:58:14 +0200

    map data

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,18 +1,24 @@
 import React from "react";
-import {
-  MapContainer,
-  TileLayer,
-  ScaleControl,
-  GeoJSON,
-  FeatureGroup,
-} from "react-leaflet";
+import { MapContainer, TileLayer, ScaleControl, GeoJSON } from "react-leaflet";
 import { useAppSelector } from "./../../app/hooks";
+//import locations from "../../data/locations.json";
+//import trails from "../../data/trails.json";
+import L from "leaflet";
 
 const MapComponent: React.FC = ({}) => {
   const zoom = useAppSelector((state) => state.map.zoom);
   const minZoom = useAppSelector((state) => state.map.minZoom);
   const maxZoom = useAppSelector((state) => state.map.maxZoom);
   const center = useAppSelector((state) => state.map.center);
+
+  /*
+  function pointToLayer(feature: any, latlng: any) {
+    var marker = L.circleMarker(latlng);
+    return marker;
+  }
+  */
+
+  //const locationsLayer: GeoJSON.FeatureCollection<any> = locations;
 
   return (
     <div
@@ -46,7 +52,6 @@ const MapComponent: React.FC = ({}) => {
         preferCanvas={true}
       >
         <ScaleControl imperial={false} position={"bottomright"} />
-
         <TileLayer
           maxNativeZoom={15}
           attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
--- src/components/map/MapSlice.ts
+++ src/components/map/MapSlice.ts
@@ -1,4 +1,4 @@
-import { createAsyncThunk, createSlice, PayloadAction } from "@reduxjs/toolkit";
+import {  createSlice } from "@reduxjs/toolkit";
 import { LatLngTuple } from "leaflet";
 
 export interface MapState {
