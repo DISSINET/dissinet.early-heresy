65c10ebeb88b1fb84c64fd0d6b27fe597a7346fa
Peter Ondrejka <pondrejk@redhat.com>
2022-04-27 18:10:33 +0200

    header style

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -5,14 +5,16 @@ const Hero: React.FC = ({}) => {
     <div
       className="hero font-bold text-xl pt-12 pb-4 px-4 text-white bg-cover"
       style={{
+        paddingTop: "2em",
         background: " cover",
+        backgroundPosition: "right",
         backgroundImage: `url(
             "https://cdn.muni.cz/media/3132847/out_gradient.png?mode=crop&center=0.54,0.86&rnd=131981761180000000&width=974&heightratio=0.23715"
           )`,
       }}
     >
-      <h2 className="heading text-white font-bold">Early Heresy in the West</h2>
-      <h4 className="subheading text-white font-medium">Suheading TBA</h4>
+      <h4>Early Heresy</h4>
+      <span>Early heresy in the west </span>
     </div>
   );
 };
