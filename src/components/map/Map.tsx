a64f8632d7336742caaae7bd99a280ed7ea61fb3
Peter Ondrejka <pondrejk@redhat.com>
2022-04-28 14:44:14 +0200

    point styling

--- src/components/map/Map.tsx
+++ src/components/map/Map.tsx
@@ -1,9 +1,16 @@
 import React from "react";
-import { MapContainer, TileLayer, ScaleControl, GeoJSON } from "react-leaflet";
+import {
+  MapContainer,
+  TileLayer,
+  ScaleControl,
+  FeatureGroup,
+  GeoJSON,
+} from "react-leaflet";
 import { useAppSelector } from "./../../app/hooks";
-//import locations from "../../data/locations.json";
-//import trails from "../../data/trails.json";
 import L from "leaflet";
+import "leaflet/dist/leaflet.css";
+import locations from "../../data/locations.json";
+import trails from "../../data/trails.json";
 
 const MapComponent: React.FC = ({}) => {
   const zoom = useAppSelector((state) => state.map.zoom);
@@ -11,14 +18,24 @@ const MapComponent: React.FC = ({}) => {
   const maxZoom = useAppSelector((state) => state.map.maxZoom);
   const center = useAppSelector((state) => state.map.center);
 
-  /*
-  function pointToLayer(feature: any, latlng: any) {
+  function setCircles(feature: any, latlng: any) {
     var marker = L.circleMarker(latlng);
     return marker;
   }
-  */
 
-  //const locationsLayer: GeoJSON.FeatureCollection<any> = locations;
+  function styleCircles(feature: any) {
+    let color = "#0000dc";
+    console.log(feature);
+    let halo = feature.properties.halo == "1" ? [10, 0.3] : [0, 1];
+    return {
+      fillColor: color,
+      radius: 4,
+      weight: halo[0],
+      opacity: halo[1],
+      color: color,
+      fillOpacity: 1,
+    };
+  }
 
   return (
     <div
@@ -52,12 +69,19 @@ const MapComponent: React.FC = ({}) => {
         preferCanvas={true}
       >
         <ScaleControl imperial={false} position={"bottomright"} />
-        <TileLayer
-          maxNativeZoom={15}
-          attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-          url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png"
-          subdomains="abcd"
-        />
+        <FeatureGroup>
+          <TileLayer
+            maxNativeZoom={15}
+            attribution='Map tiles by <a href="http://stamen.com">Stamen Design</a>, <a href="http://creativecommons.org/licenses/by/3.0">CC BY 3.0</a> &mdash; Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+            url="https://stamen-tiles-{s}.a.ssl.fastly.net/terrain-background/{z}/{x}/{y}{r}.png"
+            subdomains="abcd"
+          />
+          <GeoJSON
+            data={locations}
+            pointToLayer={setCircles}
+            style={styleCircles}
+          />
+        </FeatureGroup>
       </MapContainer>
     </div>
   );
