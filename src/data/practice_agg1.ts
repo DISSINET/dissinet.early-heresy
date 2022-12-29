06ef87625384598d81e7f9e99bf2c9de83888b23
Peter Ondrejka <pondrejk@redhat.com>
2022-12-29 13:37:54 +0100

    aggregation_level2 json

---
+++ src/data/practice_agg1.ts
@@ -0,0 +1,237 @@
+import { OutcomeAgg } from "./../types";
+
+const practice_aggregation_level2: OutcomeAgg = {
+  PAG1: {
+    id: "PAG1",
+    label: "against Church's authoritative writings",
+    members: ["C1056", "C2093", "C2222"],
+  },
+  PAG2: {
+    id: "PAG2",
+    label: "against Church's practices",
+    members: ["C1815", "C1167"],
+  },
+  PAG3: {
+    id: "PAG3",
+    label: "against cult",
+    members: [
+      "C1359",
+      "C1101",
+      "C1100",
+      "C3239",
+      "C2025",
+      "C1309",
+      "C1308",
+      "C1081",
+      "C2043",
+      "C3240",
+    ],
+  },
+
+  PAG4: {
+    id: "PAG4",
+    label: "against deed-deserved salvation",
+    members: [
+      "C1159",
+      "C2042",
+      "C1358",
+      "C2224",
+      "C1307",
+      "C2259",
+      "C2422",
+      "C2423",
+    ],
+  },
+  PAG5: {
+    id: "PAG5",
+    label: "against priestly exclusivity",
+    members: ["C2096", "C2088", "C3220"],
+  },
+  PAG6: {
+    id: "PAG6",
+    label: "gainst priestly posessions",
+    members: ["C2086", "C1618"],
+  },
+  PAG7: {
+    id: "PAG7",
+    label: "against purgatory",
+    members: ["C2260"],
+  },
+  PAG8: {
+    id: "PAG8",
+    label: "against resurrection",
+    members: ["C1357"],
+  },
+  PAG9: {
+    id: "PAG9",
+    label: "against Roman Church",
+    members: ["C1251"],
+  },
+  PAG10: {
+    id: "PAG10",
+    label: "against sacraments",
+    members: [
+      "C1061",
+      "C1064",
+      "C1113",
+      "C1065",
+      "C1082",
+      "C1153",
+      "C2087",
+      "C2095",
+      "C2046",
+    ],
+  },
+  PAG11: {
+    id: "PAG11",
+    label: "against sacred spaces",
+    members: ["C2097", "C1995", "C1152", "C2024", "C3427"],
+  },
+  PAG12: {
+    id: "PAG12",
+    label: "against social order",
+    members: ["C2091", "C2270"],
+  },
+  PAG13: {
+    id: "PAG13",
+    label: "alimentary restrictions",
+    members: ["C1073", "C1062", "C1304", "C1817", "C1066"],
+  },
+  PAG14: {
+    id: "PAG14",
+    label: "alternative Church ministers",
+    members: ["C2322", "C2324", "C2325", "C2094"],
+  },
+  PAG15: {
+    id: "PAG15",
+    label: "alternative social structure",
+    members: ["C2257"],
+  },
+  PAG16: {
+    id: "PAG16",
+    label: "anticlericalism",
+    members: ["C1994", "C1360", "C2098"],
+  },
+  PAG17: {
+    id: "PAG17",
+    label: "antiiconism",
+    members: ["C1054", "C1310"],
+  },
+  PAG18: {
+    id: "PAG18",
+    label: "apostolicity",
+    members: ["C0800"],
+  },
+  PAG19: {
+    id: "PAG19",
+    label: "asceticism/strictness",
+    members: ["C2299"],
+  },
+  PAG20: {
+    id: "PAG20",
+    label: "cult of heresiarch",
+    members: ["C3244"],
+  },
+  PAG21: {
+    id: "PAG21",
+    label: "debauchery",
+    members: ["C1818", "C1177", "C1991", "C1111"],
+  },
+  PAG22: {
+    id: "PAG22",
+    label: "deception",
+    members: ["C1996"],
+  },
+  PAG23: {
+    id: "PAG23",
+    label: "heresiarch's self-deification",
+    members: ["C1623"],
+  },
+  PAG24: {
+    id: "PAG24",
+    label: "heretical practices described",
+    members: ["C1992", "C2326", "C1242", "C3205", "C3362", "C3517"],
+  },
+  PAG25: {
+    id: "PAG25",
+    label: "heterodox attitude to Trinity",
+    members: ["C1154", "C2323", "C3250"],
+  },
+  PAG26: {
+    id: "PAG26",
+    label: "heterodox christology",
+    members: ["C1080", "C1361"],
+  },
+  PAG28: {
+    id: "PAG28",
+    label: "heterodox cosmogony",
+    members: ["C1158"],
+  },
+  PAG29: {
+    id: "PAG29",
+    label: "heterodox eschatology",
+    members: ["C2880"],
+  },
+  PAG30: {
+    id: "PAG30",
+    label: "heterodox mariology",
+    members: ["C2503"],
+  },
+  PAG31: {
+    id: "PAG31",
+    label: "heterodox sacramental theology",
+    members: ["C2489"],
+  },
+  PAG32: {
+    id: "PAG32",
+    label: "hypocrisy",
+    members: ["C1176", "C1151", "C1993"],
+  },
+  PAG33: {
+    id: "PAG33",
+    label: "in favor of deed-deserved salvation",
+    members: ["C2327", "C2269"],
+  },
+  PAG34: {
+    id: "PAG34",
+    label: "killing restrictions",
+    members: ["C1291", "C1284"],
+  },
+  PAG35: {
+    id: "PAG35",
+    label: "malpractices",
+    members: ["C1149", "C2879", "C3246"],
+  },
+  PAG36: {
+    id: "PAG36",
+    label: "martyrdom",
+    members: ["C2329", "C2330"],
+  },
+  PAG37: {
+    id: "PAG37",
+    label: "property restrictions",
+    members: ["C1306", "C2255", "C1256"],
+  },
+  PAG38: {
+    id: "PAG38",
+    label: "rules of behaviour",
+    members: ["C1255", "C1305", "C2883", "C2884", "C2885", "C2284"],
+  },
+  PAG39: {
+    id: "PAG39",
+    label: "salvation of unbaptized",
+    members: ["C2090"],
+  },
+  PAG40: {
+    id: "PAG40",
+    label: "secrecy",
+    members: ["C1150"],
+  },
+  PAG41: {
+    id: "PAG41",
+    label: "sex/marriage restrictions",
+    members: ["C1253", "C1254", "C1053", "C1816", "C2258", "C2089", "C1258"],
+  },
+};
+
+export default practice_aggregation_level2;
